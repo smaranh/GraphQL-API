0befa32e11ed415b518debae5ab65265458d18e3
Smaran Harihar <smaran.harihar@gmail.com>
2021-11-20 07:10:28 -0800

    create a basic GraphQL API

---
+++ FakeData.js
@@ -0,0 +1,46 @@
+const UserList = [
+    {
+        id: 1,
+        name: "Smaran",
+        username: "smaran",
+        age: 35,
+        nationality: "India"
+    },
+    {
+        id: 2,
+        name: "Mamta",
+        username: "mamta",
+        age: 34,
+        nationality: "India"
+    },
+    {
+        id: 3,
+        name: "Manikarnika",
+        username: "minnie",
+        age: 4,
+        nationality: "US"
+    },
+    {
+        id: 4,
+        name: "Apeksha",
+        username: "chinky",
+        age: 32,
+        nationality: "India"
+    },
+    {
+        id: 5,
+        name: "Vishal",
+        username: "vishal",
+        age: 37,
+        nationality: "India"
+    },
+    {
+        id: 6,
+        name: "Ivani",
+        username: "ivani",
+        age: 4,
+        nationality: "India"
+    }
+]
+
+module.exports = { UserList };
---
+++ schema/type-defs.js
@@ -0,0 +1,25 @@
+const {gql} = require("apollo-server");
+
+const typeDefs = gql`
+    
+    type User {
+        id: ID!
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality!
+        friends: [User]
+    }
+
+    type Query {
+        users: [User!]!
+        user(id: ID!): User!
+    }
+
+    enum Nationality {
+        India
+        US
+    }
+`
+
+module.exports = {typeDefs};
